ca1833b854aa3bb59ccadc04a57d8ac0d5c165f1
Tobias <tibistruppi.te@gmail.com>
2025-04-11 18:06:19 +0200

    alarm no alarm test

--- esp/esp.py
+++ esp/esp.py
@@ -1,9 +1,20 @@
 import socketio
+import threading
+import time
 
 socket=socketio.Client()
 
-def on_aaa_response(args):
-    print('on_aaa_response', args['data'])
+#def on_aaa_response(args):
+    #print('on_aaa_response', args['data'])
+
+def noAlarms():
+    for i in range(5):
+        socket.emit("noAlarms", "Alles in Ordnung")
+    socket.emit("Alarm", "Melder 2") 
+
+@socket.on("connect")
+def handle_connect():
+    threading.Thread(target=noAlarms, daemon=True).start()
 
 
 socket.connect("http://192.168.178.126:5000")
--- events.py
+++ events.py
@@ -3,3 +3,11 @@ def register_socket_events(socket):
     @socket.on("connect")
     def handle_connect():
         print("Connected!")
+
+    @socket.on("noAlarms")
+    def handle_noAlarms(message):
+        print(message)
+
+    @socket.on("Alarm")
+    def handle_Alarm(message):
+        print(message)
