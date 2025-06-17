8b4ceb46e700d4d62baacbaab31b910d465bd965
Tobias Eberle <tibistruppi.te@gmail.com>
2025-06-17 11:46:54 +0200

    ESP comm

--- esp/esp.py
+++ esp/esp.py
@@ -2,10 +2,10 @@ from machine import Pin, ADC
 import network
 import uasyncio as asyncio
 from dfplayer import Player
-import uwebsockets.client
+from uwebsockets.client import connect
 
 gas_analog = ADC(0)
-WS_URL = "ws://192.168.178.163:8765"
+WS_URL = "ws://192.168.19.93:8765"
 
 class Sensor:
     def __init__(self, digitalPinI, melderNrI, websocket):
@@ -30,11 +30,11 @@ class Sensor:
                 df.play(1, 2)
                 sound_active = True
             self.alarm_active = True
-            await asyncio.sleep(20)
+            await asyncio.sleep(5)
             df.stop()
             sound_active = False
 
-    async def empfangen(self):
+    '''async def empfangen(self):
         while True:
             try:
                 msg = await self.ws.recv()
@@ -43,7 +43,22 @@ class Sensor:
                     if melderNr == self.melderNr:
                         print(f"Quittierung erhalten für {melderNr}")
             except:
-                break
+                break'''
+
+async def websocket_lesen(ws, sensoren):
+    while True:
+        await asyncio.sleep(0.3)
+        try:
+            msg = await asyncio.wait_for(ws.recv(), timeout=0.3)
+            if msg.startswith("quittieren:"):
+                melderNr = msg.split(":")[1]
+                for sensor in sensoren:
+                    if sensor.melderNr == melderNr:
+                        print(f"Quittierung erhalten für {melderNr}")
+        except:
+            print(f"Fehler in websocket_lesen")
+            break
+
 
 async def analog_messen():
     while True:
@@ -60,28 +75,43 @@ async def main():
 
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
-    wlan.connect('allgaeuDSL-EB', '240606072705')
+    wlan.connect('HANDY-TOBIAS', 'Prog2IstToll')
 
     while not wlan.isconnected():
         await asyncio.sleep(0.5)
-
-    ws = await uwebsockets.client.connect(WS_URL)
-
-    await ws.send("register:1/1")
-    await ws.send("register:1/2")
+    print("WLAN verbunden")
+    try:
+        print("Versuche WebSocket-Verbindung...")
+        ws = await connect(WS_URL)
+        print("WebSocket verbunden")
+    except Exception as e:
+        print(f"Fehler beim Herstellen der WebSocket-Verbindung: {e}")
+        return
+    
+    try:
+        await ws.send("register:1/1,1/2")
+        print("Daten gesendet")
+    except Exception as e:
+        print(f"Fehler beim Daten senden: {e}")
+    
 
     sensor11 = Sensor(5, "1/1", ws)
     sensor12 = Sensor(4, "1/2", ws)
 
+    sensoren = [sensor11, sensor12]
+
     await asyncio.gather(
-        sensor11.empfangen(),
-        sensor12.empfangen(),
+        websocket_lesen(ws, sensoren),
         sensor11.messen(),
         sensor12.messen(),
         analog_messen()
     )
 
+
 try:
     asyncio.run(main())
 except Exception as e:
     print("Fehler:", e)
+    while True:
+        pass  # verhindert Neustart
+
