67904196996cf121956b73b9a06cd94775b52c6a
Tobias Eberle <tibistruppi.te@gmail.com>
2025-06-20 17:27:16 +0200

    working websocket, everything working

--- esp/esp.py
+++ esp/esp.py
@@ -5,7 +5,7 @@ from dfplayer import Player
 from uwebsockets.client import connect
 
 gas_analog = ADC(0)
-WS_URL = "ws://192.168.19.93:8765"
+WS_URL = "ws://192.168.178.163:8765"
 
 class Sensor:
     def __init__(self, digitalPinI, melderNrI, websocket):
@@ -20,44 +20,39 @@ class Sensor:
                 await self.alarm()
             else:
                 self.alarm_active = False
-            await asyncio.sleep(0.2)
+            await asyncio.sleep(0.05)
 
     async def alarm(self):
         global sound_active
         if not self.alarm_active:
             await self.ws.send(f"alarm:{self.melderNr}")
+            print(sound_active)
             if not sound_active:
                 df.play(1, 2)
                 sound_active = True
             self.alarm_active = True
-            await asyncio.sleep(5)
-            df.stop()
-            sound_active = False
-
-    '''async def empfangen(self):
-        while True:
-            try:
-                msg = await self.ws.recv()
-                if msg.startswith("quittieren:"):
-                    melderNr = msg.split(":")[1]
-                    if melderNr == self.melderNr:
-                        print(f"Quittierung erhalten für {melderNr}")
-            except:
-                break'''
+            
 
 async def websocket_lesen(ws, sensoren):
+    global sound_active
     while True:
-        await asyncio.sleep(0.3)
         try:
-            msg = await asyncio.wait_for(ws.recv(), timeout=0.3)
+            msg = await asyncio.wait_for(ws.recv(), timeout=0.1)
             if msg.startswith("quittieren:"):
                 melderNr = msg.split(":")[1]
                 for sensor in sensoren:
                     if sensor.melderNr == melderNr:
                         print(f"Quittierung erhalten für {melderNr}")
-        except:
-            print(f"Fehler in websocket_lesen")
-            break
+                        sensor.alarm_active=False
+                        df.stop()
+                        sound_active = False
+        except asyncio.TimeoutError:
+            print("Websocket Timeout Error")
+            await asyncio.sleep(0.1)  # Expliziter Task-Wechsel
+            continue 
+        except Exception as e:
+            print(f"WebSocket-Lesefehler: {e}")
+            await asyncio.sleep(1)
 
 
 async def analog_messen():
@@ -75,7 +70,7 @@ async def main():
 
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
-    wlan.connect('HANDY-TOBIAS', 'Prog2IstToll')
+    wlan.connect('allgaeuDSL-EB', '240606072705')
 
     while not wlan.isconnected():
         await asyncio.sleep(0.5)
@@ -93,7 +88,7 @@ async def main():
         print("Daten gesendet")
     except Exception as e:
         print(f"Fehler beim Daten senden: {e}")
-    
+    ws.settimeout(0.5)
 
     sensor11 = Sensor(5, "1/1", ws)
     sensor12 = Sensor(4, "1/2", ws)
@@ -104,9 +99,9 @@ async def main():
         websocket_lesen(ws, sensoren),
         sensor11.messen(),
         sensor12.messen(),
-        analog_messen()
+        analog_messen(),
+        return_exceptions=True  # Verhindert Abbruch bei Fehlern
     )
-
 
 try:
     asyncio.run(main())
