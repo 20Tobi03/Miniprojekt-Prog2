c869340c640dc3bb0e4be5354fdab950457995e6
Tobias Eberle <tibistruppi.te@gmail.com>
2025-06-20 18:01:48 +0200

    ESP cleanup and commentary

--- esp/esp.py
+++ esp/esp.py
@@ -11,24 +11,23 @@ class Sensor:
     def __init__(self, digitalPinI, melderNrI, websocket):
         self.digitalPin = Pin(digitalPinI, Pin.IN)
         self.melderNr = melderNrI
-        self.alarm_active = False
         self.ws = websocket
+        self.alarm_active = False #Mehrfach Alarmierung verhindern
+        
 
     async def messen(self):
         while True:
-            if self.digitalPin.value() == 0:
+            if self.digitalPin.value() == 0: #0 bei Alarm, 1 bei keinem alarm
                 await self.alarm()
-            else:
-                self.alarm_active = False
             await asyncio.sleep(0.05)
 
     async def alarm(self):
         global sound_active
         if not self.alarm_active:
-            await self.ws.send(f"alarm:{self.melderNr}")
-            print(sound_active)
+            await self.ws.send(f"alarm:{self.melderNr}") #websocket Alarm an Server melden
             if not sound_active:
-                df.play(1, 2)
+                await asyncio.sleep(0.1) #sonst kein Ton neustart nach quittieren
+                dfplayer.play(1, 2)
                 sound_active = True
             self.alarm_active = True
             
@@ -44,37 +43,41 @@ async def websocket_lesen(ws, sensoren):
                     if sensor.melderNr == melderNr:
                         print(f"Quittierung erhalten für {melderNr}")
                         sensor.alarm_active=False
-                        df.stop()
+                        dfplayer.stop()
                         sound_active = False
         except asyncio.TimeoutError:
             print("Websocket Timeout Error")
-            await asyncio.sleep(0.1)  # Expliziter Task-Wechsel
+            await asyncio.sleep(0.1) 
             continue 
         except Exception as e:
             print(f"WebSocket-Lesefehler: {e}")
             await asyncio.sleep(1)
 
-
+#Kontrollwert der Gas Sensoren
 async def analog_messen():
     while True:
         print("Analogwert:", gas_analog.read())
         await asyncio.sleep(1)
 
 async def main():
-    global df
+    #dfplayer mini setup
+    global dfplayer
+    dfplayer = Player()
+    dfplayer.volume(1)
     global sound_active
     sound_active = False
-    df = Player()
+    
     await asyncio.sleep(1)
-    df.volume(1)
-
+    
+    #wifi connection
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect('allgaeuDSL-EB', '240606072705')
-
     while not wlan.isconnected():
         await asyncio.sleep(0.5)
     print("WLAN verbunden")
+    
+    #websocket Verbindung aufbauen
     try:
         print("Versuche WebSocket-Verbindung...")
         ws = await connect(WS_URL)
@@ -83,18 +86,22 @@ async def main():
         print(f"Fehler beim Herstellen der WebSocket-Verbindung: {e}")
         return
     
+    #Sensoren am Server registrieren
     try:
         await ws.send("register:1/1,1/2")
         print("Daten gesendet")
     except Exception as e:
         print(f"Fehler beim Daten senden: {e}")
+    
+    #Wichtig! Sonst hängt der ESP beim lesen
     ws.settimeout(0.5)
 
+    #Sensoren lokal anlegen
     sensor11 = Sensor(5, "1/1", ws)
     sensor12 = Sensor(4, "1/2", ws)
-
     sensoren = [sensor11, sensor12]
-
+    
+    #"Multithreading" aktivieren
     await asyncio.gather(
         websocket_lesen(ws, sensoren),
         sensor11.messen(),
